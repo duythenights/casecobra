0b704333de1e97b1b1aef6a14b1ea68daef7a4c2
Richard_DinhHoangDuy <dinhoangduy@gmail.com>
2024-05-12 10:14:41 +0700

    fix: update loginlink and register link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
 import { ArrowRight } from 'lucide-react'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession()
@@ -51,23 +53,24 @@ const Navbar = async () => {
               </>
             ) : (
               <>
-                <Link
+
+                <RegisterLink
                   href='/api/auth/register'
                   className={buttonVariants({
                     size: 'sm',
                     variant: 'ghost',
                   })}>
                   Sign up
-                </Link>
+                </RegisterLink>
 
-                <Link
+                <LoginLink
                   href='/api/auth/login'
                   className={buttonVariants({
                     size: 'sm',
                     variant: 'ghost',
                   })}>
                   Login
-                </Link>
+                </LoginLink>
 
                 <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
 
